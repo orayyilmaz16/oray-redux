542243d4e853edd3ccaa74cc9c810e351710e873
Oray Yılmaz <orayyilmaz16@gmail.com>
2025-08-25 12:05:10 +0300

    oray-redux has been changed

---
+++ src/components/navi/Navi.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+
+ 
+} from 'reactstrap';
+
+export default class Navi extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen : false
+        };
+    }
+    toggle(){
+        this.setState({
+            isOpen : !this.state.isOpen
+        });
+    }
+    render()
+    {
+        return (
+            <div>
+                <Navbar color='dark' fixed='top' className='w-100' dark expand='md'>
+                    <NavbarBrand href="/">ReduxReact.Js</NavbarBrand>
+                        <NavbarToggler onClick={this.toggle} />
+                        <Collapse isOpen={this.state.isOpen} navbar>
+                            <Nav className='me-auto top' navbar>
+                            </Nav>
+                        </Collapse>
+                    </Navbar>
+            </div>
+        );
+    }
+}
--- src/components/root/App.js
+++ src/components/root/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Container } from 'reactstrap';
+import Navi from '../navi/Navi';
+import DashBoard from './DashBoard';
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Container>
+      <Navi/>
+      <DashBoard />
+    </Container>
   );
 }
 
